b9296fe8f3c6f385c4e613d8c6bfacedd448f0d2
Katleho14 <katlehomotloung394@gmail.com>
2025-09-03 18:27:48 +0200

    `Added new scripts and updated seed data in package.json and seed.js files.`

--- backend/scripts/seed.js
+++ backend/scripts/seed.js
@@ -13,13 +13,53 @@ const users = [
     email: "admin@example.com",
     password: await bcrypt.hash("admin123", 10),
     role: "admin",
-    skills: ["manage", "support"]
+    skills: ["review",
+  "support",
+  "moderation",
+  "conflict resolution",
+  "community management",
+  "communication",
+  "ticket handling",
+  "issue tracking",
+  "report analysis",
+  "user guidance",
+  "policy enforcement",
+
+  // Core Web & Programming Languages
+  "javascript",
+  "typescript",
+  "java",
+  "html",
+  "css",
+  "tailwind css"]
   },
   {
     email: "moderator@example.com",
     password: await bcrypt.hash("mod123", 10),
     role: "moderator",
-    skills: ["review", "support"]
+    skills:[
+  // Moderator / Support
+  "review",
+  "support",
+  "moderation",
+  "conflict resolution",
+  "community management",
+  "communication",
+  "ticket handling",
+  "issue tracking",
+  "report analysis",
+  "user guidance",
+  "policy enforcement",
+
+  // Core Web & Programming Languages
+  "javascript",
+  "typescript",
+  "java",
+  "html",
+  "css",
+  "tailwind css"
+]
+
   },
   {
     email: "user@example.com",
