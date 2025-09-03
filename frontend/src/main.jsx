a5ea1646041511670ed69f779f48ba97909da0fe
Katleho14 <katlehomotloung394@gmail.com>
2025-09-03 08:56:55 +0200

    feat: add TicketsContext for managing ticket state and fetching logic
    - Introduced TicketsContext to manage tickets, pagination, and loading state.
    - Updated main.jsx to wrap routes with TicketsProvider and added Navbar component.
    - Added react-markdown dependency to package.json.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,60 +1,65 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CheckAuth from "./components/check-auth.jsx";
+import Navbar from "./components/navbar.jsx";
 import Tickets from "./pages/tickets.jsx";
 import TicketDetailsPage from "./pages/ticket.jsx";
 import Login from "./pages/login.jsx";
 import Signup from "./pages/signup.jsx";
 import Admin from "./pages/admin.jsx";
+import { TicketsProvider } from "./contexts/TicketsContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <CheckAuth protected={true}>
-              <Tickets />
-            </CheckAuth>
-          }
-        />
-        <Route
-          path="/tickets/:id"
-          element={
-            <CheckAuth protected={true}>
-              <TicketDetailsPage />
-            </CheckAuth>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <CheckAuth protected={false}>
-              <Login />
-            </CheckAuth>
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <CheckAuth protected={false}>
-              <Signup />
-            </CheckAuth>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <CheckAuth protected={true}>
-              <Admin />
-            </CheckAuth>
-          }
-        />
-      </Routes>
+      {/* Provide tickets context to all routes under \"Tickets\" and \"Admin\" */}
+      <TicketsProvider>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <CheckAuth protectedRoute={true}>
+                <Tickets />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path="/tickets/:id"
+            element={
+              <CheckAuth protectedRoute={true}>
+                <TicketDetailsPage />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <CheckAuth protectedRoute={false}>
+                <Login />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <CheckAuth protectedRoute={false}>
+                <Signup />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <CheckAuth protectedRoute={true}>
+                <Admin />
+              </CheckAuth>
+            }
+          />
+        </Routes>
+      </TicketsProvider>
     </BrowserRouter>
   </StrictMode>
 );
