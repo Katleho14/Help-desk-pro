3c7f35a3ed5ccacfb5b259f9a99b4fe5a9cc2c1d
Katleho14 <katlehomotloung394@gmail.com>
2025-09-09 20:10:53 +0200

    `Fixed getTicket controller: updated Ticket.findById and Ticket.findOne queries with await keyword and modified response status and data`

--- backend/controller/ticket.js
+++ backend/controller/ticket.js
@@ -83,21 +83,21 @@ export const getTicket = async (req, res) => {
     let ticket;
 
     if (user.role !== "user") {
-      ticket = Ticket.findById(req.params.id).populate("assignedTo", [
+      ticket = await Ticket.findById(req.params.id).populate("assignedTo", [
         "email",
         "_id",
       ]);
     } else {
-      ticket = Ticket.findOne({
+      ticket = await Ticket.findOne({
         createdBy: user._id,
         _id: req.params.id,
-      }).select("title description status createdAt");
+      }).select("title description status createdAt priority");
     }
 
     if (!ticket) {
       return res.status(404).json({ message: "Ticket not found" });
     }
-    return res.status(404).json({ ticket });
+    return res.status(200).json({ ticket });
   } catch (error) {
     console.error("Error fetching ticket", error.message);
     return res.status(500).json({ message: "Internal Server Error" });
