a91da1cde65372a16f1717ddaa61f2f14e8b48ec
Katleho14 <katlehomotloung394@gmail.com>
2025-10-02 13:47:16 +0200

    `Added summary field to ticket schema`

--- backend/models/ticket.js
+++ backend/models/ticket.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const ticketSchema = new mongoose.Schema({
   title: String,
   description: String,
+  summary: String,
   status: { type: String, default: "TODO" },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   assignedTo: {
